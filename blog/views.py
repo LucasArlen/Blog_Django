98bd1502aa6d932f922e88781128c76931305859
LucasArlen <portgas.arlen@gmail.com>
2021-09-25 09:28:11 -0300

    BlogDjango

---
+++ blog/views.py
@@ -0,0 +1,143 @@
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from django.core.mail import send_mail
+from django.db.models import Count
+from mysite import settings
+
+from taggit.models import Tag
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm, SearchForm
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    
+    # Lista de comentários ativos para esta postagem
+    comments = post.comments.filter(active=True)
+    
+    new_comment = None
+    
+    if request.method == 'POST':
+        # Um comentário foi postado
+        comment_form = CommentForm(data=request.POST)
+        
+        if comment_form.is_valid():
+            # Cria o objeto comment, mas não
+            # o salva ainda no banco de dados
+            new_comment = comment_form.save(commit=False)
+            # Atribui a postagem atual ao comentaŕio
+            new_comment.post = post
+            # Salva o comentário no banco de dados
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+        
+        
+        
+    # Lista de postagens semelhantes
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                           .order_by('-same_tags', '-publish')[:4]
+
+    return render(request, 'blog/post/detail.html',
+                            {'post': post,
+                            'comments': comments,
+                            'new_comment': new_comment,
+                            'comment_form': comment_form,
+                            'similar_posts': similar_posts})
+
+
+def post_share(request, post_id):
+    # Obtém a postagen com base no Id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    
+    if request.method == 'POST':
+        # Formulário foi submetido
+        form = EmailPostForm(request.POST)
+        
+        if form.is_valid():
+            # Campos do formulário passsam por validação    
+            cd = form.cleaned_data
+            # ... Envia o email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read"\
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n"\
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, settings.EMAIL_HOST_USER, [cd['to']])
+            sent = True
+                      
+    else:
+        form = EmailPostForm()
+        
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
+
+
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    
+    paginator = Paginator(object_list, 3) # Três postagens em cada página
+    page = request.GET.get('page')
+    
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Se a página não for um inteiro, exibe a primeira página
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Se a página estiver fora do intervalo
+        # exibe a última página de resultados
+        posts = paginator.page(paginator.num_pages)
+    
+    return render(request, 'blog/post/list.html',
+                            {'page': page,
+                             'posts': posts,
+                             'tag': tag})
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body', 'slug')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
+    
+    context = {'form': form,
+                'query': query,
+                'results:': results}
+    
+    return render(request, 'blog/post/search.html', context)
+                  
+    
+    
+    
+"""
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+"""
